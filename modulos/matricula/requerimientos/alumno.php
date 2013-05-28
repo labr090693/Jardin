c1196cbf0bf5a4e02fb1677d544a6ba9da134153
labr090693 <alexander_x10@hotmail.com>
2013-05-28 09:21:23 -0500

    octavo commit

--- modulos/matricula/requerimientos/alumno.php
+++ modulos/matricula/requerimientos/alumno.php
@@ -1,137 +1,149 @@
 <form class="tabla2">
-                   <table>
-                        <tr>
-                           <td>Lengua Materna</td>
-                           <td><input name="lenguamaterna" type="text" >
-                        </tr>
-                        <tr>
-                           <td>Lengua Actual</td>
-                           <td><input name="lenguaaactual" type="text" >
-                        </tr>
-                        <tr>
-                            <td>N&uacute;mero Hermanos</td>
-                            <td><input class="input-mini" name="numerohermanos" type="text" >
-                        </tr>
-                        <tr>
-                            <td>Lugar que ocupa</td>
-                            <td><input class="input-medium" name="lugarocupa" type="text" >
-                        </tr>
-                        <tr>
-                            <td>Religi&oacute;n</td>
-                            <td><input name="religion" type="text" >
-                        </tr>
-                        <tr>
-                            <td>Necesidades especiales</td>
-                            <td>
-                                <label class="checkbox inline">
-                                DI<input name="di" type="checkbox">
-                                </label>
-                                <label class="checkbox inline">
-                                DA<input name="da" type="checkbox">
-                                </label>
-                                <label class="checkbox inline">
-                                DV<input name="dv" type="checkbox">
-                                </label>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>N. Documento</td>
-                            <td><input name="numerodocumento" type="text" >
-                        </tr>
-                   </table>
-               </form>
+      <table>
+             <tr>
+                 <td>Apellido Paterno</td>
+                 <td><input name="apellidop" type="text" size="30" placeholder="Escriba su apellido paterno"></td>
+             </tr>
+             <tr>
+                 <td>Apellido Materno</td>
+                 <td><input name="apellidom" type="text" size="30" width="10px;" placeholder="Escriba su apellido materno"></td>
+             </tr>
+             <tr>
+                 <td>Nombres</td>
+                 <td><input name="nombres" type="text" size="30" placeholder="Escriba sus nombres"></td>
+             </tr>
+             <tr>
+                 <td>Sexo</td>
+                 <td>
+                     <select>
+                           <option value="">Seleccionar...</option>
+                           <option value="M">Masculino</option>
+                           <option value="F">Femenino</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>Estado Civil</td>
+                 <td>
+                     <select>
+                            <option value="">Seleccionar...</option>
+                            <option value="1">Soltero</option>
+                            <option value="2">casado</option>
+                            <option value="3">Divorsiado</option>
+                     </select>
+                 </td>
+             </tr>
+      </table>
+</form>
+
+
+
+<form class="tabla2">
+      <table>
+             <tr>
+                 <td>Fecha de nacimiento</td>
+                 <td>
+                     <input class="input-mini" name="dia" type="text" size="2" placeholder="dia">/
+                     <input class="input-mini" name="mes" type="text" size="2" placeholder="mes">/
+                     <input class="input-mini" name="anio" type="text" size="2" placeholder="anio">
+                 </td>
+             </tr>
+             <tr>
+                 <td>Pa&iacute;s</td>
+                 <td>
+                     <select>
+                     <option value="">Seleccionar...</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>Departamento</td>
+                 <td>
+                     <select>
+                     <option value="">Seleccionar...</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>Provincia</td>
+                     <td><select>
+                     <option value="">Seleccionar...</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>Distrito</td>
+                 <td>
+                     <select>
+                     <option value="">Seleccionar...</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>Documento Ident.</td>
+                 <td>
+                      <label class="checkbox inline">
+                      DNI<input name="dni" type="checkbox">
+                      </label>
+                      <label class="checkbox inline">
+                      LE<input name="le" type="checkbox">
+                      </label>
+                      <label class="checkbox inline">
+                      CE<input name="ce" type="checkbox">
+                      </label>
+                 </td>
+             </tr>
+      </table> 
+</form>
+
+
+
+<form class="tabla2">
+      <table>
+             <tr>
+                 <td>Lengua Materna</td>
+                 <td><input name="lenguamaterna" type="text" >
+             </tr>
+             <tr>
+                 <td>Lengua Actual</td>
+                 <td><input name="lenguaaactual" type="text" >
+             </tr>
+             <tr>
+                 <td>N&uacute;mero Hermanos</td>
+                 <td><input class="input-mini" name="numerohermanos" type="text" >
+             </tr>
+             <tr>
+                 <td>Lugar que ocupa</td>
+                 <td><input class="input-medium" name="lugarocupa" type="text" >
+             </tr>
+             <tr>
+                 <td>Religi&oacute;n</td>
+                 <td><input name="religion" type="text" >
+             </tr>
+             <tr>
+                 <td>Necesidades especiales</td>
+                 <td>
+                     <label class="checkbox inline">
+                     DI<input name="di" type="checkbox">
+                     </label>
+                     <label class="checkbox inline">
+                     DA<input name="da" type="checkbox">
+                     </label>
+                     <label class="checkbox inline">
+                     DV<input name="dv" type="checkbox">
+                     </label>
+                 </td>
+              </tr>
+              <tr>
+                  <td>N. Documento</td>
+                  <td><input name="numerodocumento" type="text" >
+              </tr>
+       </table>
+</form>
                
                
                
                
-               <form class="tabla2">
-                    <table>
-                        <tr>
-                           <td>Fecha de nacimiento</td>
-                           <td><input class="input-mini" name="dia" type="text" size="2" placeholder="dia">/
-                               <input class="input-mini" name="mes" type="text" size="2" placeholder="mes">/
-                               <input class="input-mini" name="anio" type="text" size="2" placeholder="anio">
-                           </td>
-                        </tr>
-                        <tr>
-                            <td>Pa&iacute;s</td>
-                            <td><select>
-                                    <option value="">Seleccionar...</option>
-                               </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Departamento</td>
-                            <td><select>
-                                    <option value="">Seleccionar...</option>
-                               </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Provincia</td>
-                            <td><select>
-                                    <option value="">Seleccionar...</option>
-                               </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Distrito</td>
-                            <td><select>
-                                    <option value="">Seleccionar...</option>
-                               </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Documento Ident.</td>
-                            <td>
-                                <label class="checkbox inline">
-                                DNI<input name="dni" type="checkbox">
-                                </label>
-                                <label class="checkbox inline">
-                                LE<input name="le" type="checkbox">
-                                </label>
-                                <label class="checkbox inline">
-                                CE<input name="ce" type="checkbox">
-                                </label>
-                            </td>
-                        </tr>
-                    </table> 
-               </form>
                
-               <form class="tabla1">
-                   <table>
-                       <tr>
-                           <td>Apellido Paterno</td>
-                           <td><input name="apellidop" type="text" size="30" placeholder="Escriba su apellido paterno"></td>
-                       </tr>
-                       <tr>
-                           <td>Apellido Materno</td>
-                           <td><input name="apellidom" type="text" size="30" width="10px;" placeholder="Escriba su apellido materno"></td>
-                       </tr>
-                       <tr>
-                           <td>Nombres</td>
-                           <td><input name="nombres" type="text" size="30" placeholder="Escriba sus nombres"></td>
-                       </tr>
-                       <tr>
-                           <td>Sexo</td>
-                           <td>
-                               <select>
-                               <option value="">Seleccionar...</option>
-                               <option value="M">Masculino</option>
-                               <option value="F">Femenino</option>
-                               </select>
-                           </td>
-                       </tr>
-                       <tr>
-                           <td>Estado Civil</td>
-                           <td>
-                               <select>
-                               <option value="">Seleccionar...</option>
-                               <option value="1">Soltero</option>
-                               <option value="2">casado</option>
-                               <option value="3">Divorsiado</option>
-                               </select>
-                           </td>
-                       </tr>
-                   </table>
-              </form>
+               
+               
